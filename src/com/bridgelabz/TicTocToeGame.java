429f122def1b08c9e7bd7b44d0e4107c4bf6c695
Aniket-123-bit <anisunny782@gmail.com>
2021-06-30 22:15:30 +0530

    Commit UC2 Push

--- src/com/bridgelabz/TicTocToeGame.java
+++ src/com/bridgelabz/TicTocToeGame.java
@@ -1,14 +1,78 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+import java.util.ArrayList;
 public class TicTocToeGame {
-    public static void main(String args []){
-                  char[] board = new char[10];
-            for(int i = 0; i < board.length; i++) {
-                board[i] = '-';
+    static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
+    static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();
+
+    public static void printGameBoard(char[][] gameBoard) {
+        for (char[] row : gameBoard) {
+            for (char c : row) {
+                System.out.print(c);
             }
-            System.out.println(board);
+            System.out.println();
         }
     }
+
+    public static void place(char[][] gameBoard, int position, String user) {
+        char symbol = ' ';
+        if (user.equalsIgnoreCase("player")) {
+            symbol = 'X';
+            playerPositions.add(position);
+        } else if (user.equalsIgnoreCase("CPU")) {
+            symbol = 'O';
+            cpuPositions.add(position);
+        }
+
+        switch (position) {
+            case 1:
+                gameBoard[0][0] = symbol;
+                break;
+            case 2:
+                gameBoard[0][2] = symbol;
+                break;
+            case 3:
+                gameBoard[0][4] = symbol;
+                break;
+            case 4:
+                gameBoard[2][0] = symbol;
+                break;
+            case 5:
+                gameBoard[2][2] = symbol;
+                break;
+            case 6:
+                gameBoard[2][4] = symbol;
+                break;
+            case 7:
+                gameBoard[4][0] = symbol;
+                break;
+            case 8:
+                gameBoard[4][2] = symbol;
+                break;
+            case 9:
+                gameBoard[4][4] = symbol;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[][] gameBoard = {{' ', '|', ' ', '|', ' '}, {'-', '+', '-', '+', '-'}, {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'}, {' ', '|', ' ', '|', ' '}};
+        printGameBoard(gameBoard);
+
+
+    }
+}
+
+
+
+
+
 
 
 
