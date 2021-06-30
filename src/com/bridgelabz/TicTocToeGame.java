631f0c06e7e18249625948c23b7ef0c24742fb9f
Aniket-123-bit <anisunny782@gmail.com>
2021-06-30 20:16:37 +0530

    Commit UC1 Push

---
+++ src/com/bridgelabz/TicTocToeGame.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class TicTocToeGame {
+    public static void main(String args []){
+                  char[] board = new char[10];
+            for(int i = 0; i < board.length; i++) {
+                board[i] = '-';
+            }
+            System.out.println(board);
+        }
+    }
+
+
+
+
